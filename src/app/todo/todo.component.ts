9f1fb3efbe20b461f808af5fb12929120dbcd62a
Alaina Balducci <alainabalducci@gmail.com>
2023-06-22 11:11:18 -0500

    fixed import issue

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,11 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ITask } from '../model/Task';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
@@ -9,27 +13,30 @@ import { ITask } from '../model/Task';
   styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-
-  todoForm !: FormGroup;
-  tasks : ITask [] = [];
-  inprogress : ITask [] = [];
-  done : ITask [] = [];
-  constructor(private fb : FormBuilder) {}
+  todoForm!: FormGroup;
+  tasks: ITask[] = [];
+  inprogress: ITask[] = [];
+  done: ITask[] = [];
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.todoForm = this.fb.group({
-      item : ['', Validators.required]
-    })
+      item: ['', Validators.required],
+    });
   }
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
